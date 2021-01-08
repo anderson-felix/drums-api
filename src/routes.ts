6f78a4701fedc70c17a3c8d23bb86076735bf541
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-01-07 22:49:59 -0300

    create and update

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 
 import * as SessionController from './controllers/Session';
+import * as MediaController from './controllers/Media';
 import auth from './middlewares/auth';
 
 const routes = Router();
 
 routes.post('/register', SessionController.store);
-routes.post('/session', SessionController.login);
 
+routes.post('/session', SessionController.login);
 routes.get('/session', auth, SessionController.getLogged);
 routes.put('/session', auth, SessionController.update);
+
 routes.post('/logout', auth, SessionController.Logout);
+
+routes.post('/media', MediaController.store);
+routes.put('/media', MediaController.update);
 
 export default routes;
